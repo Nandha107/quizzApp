1802181ef443c958483c95fe6507c261d81cd9f9
Nandha107 <nandha10799@gmail.com>
2024-09-30 18:24:51 +0530

    responsive bug

--- src/component/layouts/authLayout.tsx
+++ src/component/layouts/authLayout.tsx
@@ -17,30 +17,23 @@ export const AuthLayout: React.FC<AuthLayout> = ({}) => {
 			navigate('/', { replace: true });
 			return;
 		}
-
-		// if (!isLoggedIn) navigate('/logout', { replace: true });
 	}, []);
 
 	if (!isLoggedIn)
 		return <LoadingSpinner text="You are being logged out" className="text-teal-600" />;
 
 	return (
-		<div className="flex flex-col border-4 w-screen h-screen border-red-900">
-			<div className="h-[7%] w-full px-2 md:px-5 shadow-lg bg-white z-[1000]">
+		<div className="h-screen flex flex-col border-2 border-red-900">
+			<div className="px-2 md:px-5 shadow-lg bg-white z-[1000]">
 				<Header />
 			</div>
-			{/* <div className="w-full"> */}
-			{/* <div className="h-[93%] w-full"> */}
-				<div className="w-full h-full transition-all duration-700 bg-slate-600/5">
-					<Suspense
-						fallback={
-							<LoadingSpinner text="Please wait while loading goodies..." />
-						}
-					>
-						<Outlet />
-					</Suspense>
-				</div>
-			{/* </div> */}
+			<div className="h-full transition-all duration-700 bg-slate-600/5">
+				<Suspense
+					fallback={<LoadingSpinner text="Please wait while loading goodies..." />}
+				>
+					<Outlet />
+				</Suspense>
+			</div>
 		</div>
 	);
 };
@@ -60,3 +53,49 @@ export const AuthLayout: React.FC<AuthLayout> = ({}) => {
 // 		</div>
 // 	</div>
 // </div>
+
+// import { Suspense, useEffect } from 'react';
+// import { Outlet, useNavigate } from 'react-router-dom';
+// import { LoadingSpinner } from '../spinner/loadingSpinner';
+// import Header from '../Navigation/header';
+// import { Config } from '../../config';
+// // import { jwtDecode } from 'jwt-decode';
+
+// interface AuthLayout {}
+
+// export const AuthLayout: React.FC<AuthLayout> = ({}) => {
+// 	const navigate = useNavigate();
+
+// 	const isLoggedIn = localStorage.getItem(Config.localStorageKeys.access_token);
+
+// 	useEffect(() => {
+// 		if (!isLoggedIn) {
+// 			navigate('/', { replace: true });
+// 			return;
+// 		}
+
+// 		// if (!isLoggedIn) navigate('/logout', { replace: true });
+// 	}, []);
+
+// 	if (!isLoggedIn)
+// 		return <LoadingSpinner text="You are being logged out" className="text-teal-600" />;
+
+// 	return (
+// 		<div className="flex flex-col border w-screen h-screen">
+// 			<div className="h-[7%] w-full px-2 md:px-5 shadow-lg bg-white z-[1000]">
+// 				<Header />
+// 			</div>
+// 			<div className="h-[93%] w-full">
+// 				<div className="w-full h-full transition-all duration-700 bg-slate-600/5">
+// 					<Suspense
+// 						fallback={
+// 							<LoadingSpinner text="Please wait while loading goodies..." />
+// 						}
+// 					>
+// 						<Outlet />
+// 					</Suspense>
+// 				</div>
+// 			</div>
+// 		</div>
+// 	);
+// };
