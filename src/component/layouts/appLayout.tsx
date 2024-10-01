9416ba9ab6ffb8f9a39f09b3c4f9d02b346b6253
Nandha107 <nandha10799@gmail.com>
2024-10-01 17:09:29 +0530

    responsive bug fixing

--- src/component/layouts/appLayout.tsx
+++ src/component/layouts/appLayout.tsx
@@ -5,7 +5,7 @@ interface AppLayout {}
 
 export const AppLayout: React.FC<AppLayout> = ({}) => {
 	return (
-		<div className="w-screen h-screen border-4 border-red-900 overflow-y-auto">
+		<div className="w-screen h-screen border-4 border-red-900 overflow-hidden">
 			<Toast />
 			<Outlet />
 		</div>
