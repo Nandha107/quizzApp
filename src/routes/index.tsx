1f5ad534fb35eb5c6b45098c743f6edb951eda11
Nandha107 <nandha10799@gmail.com>
2024-10-09 00:45:38 +0530

    bug fixing and old create test related files deleted

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,8 +5,6 @@ import StaffDashboard from '../pages/admin/dashboard';
 import StudentDashboard from '../pages/student/dashboard';
 import AssessmentDetails from '../component/assessment/assessmentDetails';
 import CoursesPage from '../pages/admin/coursesPage';
-import CreateTestPage from '../pages/admin/createTest';
-// import AssessmentCreateModal from '../component/assessment/assessmentCreateModal';
 import QuestionPage from '../pages/student/questionPage';
 import ResultPage from '../pages/student/ResultPage';
 import { insecureRoutes } from './insecureRoutes';
@@ -33,14 +31,6 @@ export const router = createBrowserRouter([
 					{
 						path: 'create-assessment/:dept',
 						element: <CreateAssessment />,
-					},
-					// {
-					// 	path: 'create-assessment/:dept',
-					// 	element: <AssessmentCreateModal />,
-					// },
-					{
-						path: 'staff-dashboard/:dept/:assessmentId/create-level',
-						element: <CreateTestPage />,
 					},
 					{
 						path: 'assessment-analytics/:assessmentId',
